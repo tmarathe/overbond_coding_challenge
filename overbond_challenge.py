79ea4d9d879fa04d2ef6affb4244da1170764e36
Tarang <tarang.marathe@gmail.com>
2017-03-03 20:01:27 -0500

    challenge 1 implementation complete

--- overbond_challenge.py
+++ overbond_challenge.py
@@ -1,5 +1,5 @@
-import numpy as np
 import csv
+import numpy as np
 
 # reads data from the specified csv
 # returns a dictionary {bond:[attributes]}
@@ -8,14 +8,36 @@ def read_data(filename):
     with open(filename, 'r') as file:
         reader = csv.reader(file)
         next(reader, None) # to skip the header
-        # term and yield are cast to floats in the attribute list
-        bond_dict = {row[0]:[row[1], float(row[2].split(" ")[0]), \
-                             float(row[3].split("%")[0])] for row in reader}
-        print(bond_dict)
+        corporate, government = {}, {}
+        for row in reader:
+            if row[1] == "corporate":
+                # term and yield are cast to floats in the attribute list
+                corporate[row[0]] = [float(row[2].split(" ")[0]), float(row[3].split("%")[0])]
+            elif row[1] == "government":
+                government[row[0]] = [float(row[2].split(" ")[0]), float(row[3].split("%")[0])]
+        return corporate, government
 
+
+def yield_spread(corp, gov, outfile):
+    # create two lists (term length, and bond names) with one pass
+    benchmark_terms, benchmark_bonds = zip(*[(gov[g][0], g) for g in gov])
+    yield_spread_dict = {}
+    for bond in corp:
+        i = (np.abs(np.array(benchmark_terms) - corp[bond][0])).argmin()
+        benchmark = benchmark_bonds[i]
+        spread = np.abs(corp[bond][1] - gov[benchmark][1])
+        yield_spread_dict[bond] = [benchmark, str(spread)+"%"]
+
+    with open(outfile, 'w') as csvfile:
+        csv_writer = csv.writer(csvfile, delimiter=',')
+        csv_writer.writerow(("bond", "benchmark", "spread_to_benchmark"))
+        for k, v in yield_spread_dict.items():
+            csv_writer.writerow([k] + v)
 
 
 if __name__ == "__main__":
 
-    input_file = "small_input.csv"
-    read_data(input_file)
+    input_file = "sample_input.csv"
+    corp, gov = read_data(input_file)
+    #print(corp, gov)
+    yield_spread(corp, gov, "test_out.csv")
