0fc8fb7c19566abfb490c48c5b838d38d8172801
Tarang <tarang.marathe@gmail.com>
2017-03-03 23:29:32 -0500

    added tests, completed README

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# overbond_coding_challenge
-Solution for the Overbond coding challenge
+# Overbond Coding Challenge
+## By Tarang Marathe
+
+The file `overbond_challenge.py` contains all the functions required to solve the two exercises. Code is written in Python 3, and the required libraries are `csv` (to read and write .csv files), `numpy`, `scipy` (for various mathematical functions), and `difflib` for testing the generated files.
+
+The function `read_data()` takes a filename/path of a source .csv file and and returns two Python dictionary whose keys are the unique bond ids (C1, G1, etc), and values are a list of attributes (term, yield) of the corresponding bond. This data structure was chosen because lookup from a hashtable/dictionary is amortized O(1). One of the dictionaries returned contains the information for corporate bonds, the other is for government bonds.
+
+The function `yield_spread()` calculates the yield spread for each corporate bond by searching the dictionary of government bonds for the the one with the closest term to maturity, and computing the difference in yields. If the corporate bond dictionary has *m* entries the government bond dictionary has *n*, this operation has a worst-case of O(mn).
+
+This could be improved - given more time - if we implemented a self-balancing binary tree data structure (AVL, Red-Black) that sorted bonds based on their term. In this case, searching for the government bond with nearest term for each corporate bond within the binary tree would have a worst-case of O(m log n). 
+
+Similarly, the `spread_to_curve()` function calculates the spread to the government bond curve for each corporate bond. The government bond curve is calculated using the `scipy` function `interpolate.interp1d`. This curve can be used to find the interpolated yield value for the term of each corporate bond, and the difference of these terms is the required spread to curve.
+
+Finally, the `write_data()` function takes a dictionary data structure computed by either `yield_spread()` or `spread_to_curve()` and writes the data to a .csv file.
+
+Some tests have been included in the `__main__` scope. 
+
+Note: If you run `overbond_challenge.py`, you will need to keep the folder `required_files` in the same directory, as the test compare generated files to those in the folder (which were calculated manually). Alternatively, you can comment out the tests and call the functions on files of your own.
--- overbond_challenge.py
+++ overbond_challenge.py
@@ -1,9 +1,10 @@
 import csv
 import numpy as np
 from scipy import interpolate
+import difflib
 
 # reads data from the specified csv
-# returns a dictionary {bond:[attributes]}
+# returns a dictionary of the form: { bond : [ term, yield ] }
 # assumes a well formed input - no error checking done on the csv
 def read_data(filename):
     with open(filename, 'r') as file:
@@ -18,7 +19,11 @@ def read_data(filename):
                 government[row[0]] = [float(row[2].split(" ")[0]), float(row[3].split("%")[0])]
         return corporate, government
 
-
+# takes two dictionaries - corporate bonds and government bonds
+# returns a dictionary of the form: { bond : [ benchmark, difference ] }
+# 'bond' is a coporate bond
+# 'benchmark' is the id of the government bond with nearest term to 'bond'
+# 'difference' is the difference between the 'bond' yield and 'benchmark' yield
 def yield_spread(corp, gov):
     # create two lists (term length, and bond names) with one pass
     benchmark_terms, benchmark_bonds = zip(*[(gov[g][0], g) for g in gov])
@@ -26,14 +31,15 @@ def yield_spread(corp, gov):
     for bond in corp:
         i = (np.abs(np.array(benchmark_terms) - corp[bond][0])).argmin()
         benchmark = benchmark_bonds[i]
-        spread = np.abs(corp[bond][1] - gov[benchmark][1])
-        yield_spread_dict[bond] = [benchmark, round(spread, 2)]
+        difference = corp[bond][1] - gov[benchmark][1]
+        yield_spread_dict[bond] = [benchmark, round(difference, 2)]
     return yield_spread_dict
 
-
+# returns a dictionary of the form { bond : [spread] }
+# 'bond' is a corporate bond, 'spread' is the spread to curve
+# government bond curve is calculated using linear interpolation
 def spread_to_curve(corp, gov):
     gov_terms, gov_yields = zip(*[(gov[g][0], gov[g][1]) for g in gov])
-
     gov_curve = interpolate.interp1d(gov_terms, gov_yields)
     interpolated_yields = {}
     for c in corp:
@@ -46,9 +52,9 @@ def spread_to_curve(corp, gov):
 def write_data(data_dict, data_type, outfile):
     with open(outfile, 'w') as csvfile:
         csv_writer = csv.writer(csvfile, delimiter=',')
-        if data_type == "spread":
+        if data_type == "benchmark":
             csv_writer.writerow(("bond", "benchmark", "spread_to_benchmark"))
-        elif data_type == "interpolated":
+        elif data_type == "curve":
             csv_writer.writerow(("bond", "spread_to_curve"))
         for k, v in data_dict.items():
             csv_writer.writerow([k] + v)
@@ -56,11 +62,33 @@ def write_data(data_dict, data_type, outfile):
 
 if __name__ == "__main__":
 
-    input_file = "sample_input.csv"
-    corp, gov = read_data(input_file)
+    # NOTE: If you run this file, you will need to keep the folder required_files in the same directory
+    # the test compare generated files to those in the folder (which were calculated manually)
 
-    spread = yield_spread(corp, gov)
-    write_data(spread, "spread", "sample_yield.csv")
+    # Tests
+    input_files = ["small_input.csv", "sample_input.csv"]
+    for i in input_files:
+        corp, gov = read_data(i)
+        i_name = i.split("_")[0]
+        spread = yield_spread(corp, gov)
+        write_data(spread, "benchmark", "generated_files/" + i_name + "_yield.csv")
 
-    intp = spread_to_curve(corp, gov)
-    write_data(intp, "interpolated", "sample_intp_yield.csv")
+        intp = spread_to_curve(corp, gov)
+        write_data(intp, "curve", "generated_files/" + i_name + "_intp_yield.csv")
+
+    generated = ["generated_files/small_yield.csv", "generated_files/small_intp_yield.csv", \
+                 "generated_files/sample_yield.csv", "generated_files/sample_intp_yield.csv"]
+
+    required = ["required_files/small_yield_required.csv", "required_files/small_intp_yield_required.csv", \
+                "required_files/sample_yield_required.csv", "required_files/sample_intp_yield_required.csv"]
+
+
+    for g, r in zip(generated, required):
+        test = open(g).readlines()
+        correct = open(g).readlines()
+
+        diff = difflib.unified_diff(test, correct)
+        if "".join(diff) == "":
+            print("test passed")
+        else:
+            print("test failed")
